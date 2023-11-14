99aaf1d3999ddf29826b0fb294aa92d32bd7dce3
Pritam <pritam@codewithme.com>
2023-11-14 11:25:41 +0530

    update commit

--- src/components/layout/Dboxes.js
+++ src/components/layout/Dboxes.js
@@ -5,10 +5,10 @@ const Dboxes = (props) => {
   return (
     <div className="dbox">
   
-            <div class="card" >
-              <div class="card-body">
-                <h5 class="card-title">{props.title}</h5>
-                <p class="card-text">{props.content}</p>
+            <div className="card" >
+              <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.content}</p>
                 <button className="cardbtn">Read more</button>
               </div>
             </div>
